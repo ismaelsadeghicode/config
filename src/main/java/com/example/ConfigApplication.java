7bdde880b5abda20ca400464a3600a9abf6bd832
ismaelsadeghicode <ismaelsadeghicode@gmail.com>
2020-01-26 22:23:05 +0330

    create project config service

---
+++ src/main/java/com/example/ConfigApplication.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ismael Sadeghi, 1/26/20 10:19 PM
+ */
+@SpringBootApplication
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
